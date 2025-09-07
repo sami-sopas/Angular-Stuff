7e9db021f78b95612af1b8f56fd3854286dea26a
Samuel Cuenca <99571985+sami-sopas@users.noreply.github.com>
2025-09-06 18:53:00 -0600

    Update 03-functions.ts

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -17,10 +17,34 @@ function multiply(
   return firstNumber * base;
 }
 
+/*
 const result: number = addNumbers(1, 2);
 const result2: string = addNumbersArrow(1, 2);
 const multiplyResult: number = multiply(5);
 
 console.log({ result, result2, multiplyResult });
+*/
 
+//Pasar parametros como objeto a una funcion
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider = {
+  name: 'Strider',
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida: ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 50);
+
+strider.showHp();
 export {};
