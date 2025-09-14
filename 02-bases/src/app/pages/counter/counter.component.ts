a1ddfd4e845662074c0f8bf1ae7c34275f58cf2c
sami-sopas <ramirezcuenca@outlook.es>
2025-09-14 17:35:31 -0600

    separate into tempalte

--- 02-bases/src/app/pages/counter/counter.component.ts
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -1,15 +1,28 @@
 import { Component } from "@angular/core";
 
 @Component({
-  template: `
-    <h1>Counter: {{ counter }}</h1>
-    <button (click)="increaseByOne(1)">+1</button>
-  `,
+  templateUrl: './counter.component.html',
+  styles: `
+  button {
+    padding: 5px;
+    margin: 5px 10px;
+    width: 75px:
+  }`
 })
 export class CounterComponent {
   counter = 15;
 
   increaseByOne(value: number) {
-    this.counter += value;
+    console.log(value)
+    if(value > 0){
+      this.counter += value;
+    }
+    else{
+      this.counter -= Math.abs(value);
+    }
+  }
+
+  resetCounter(){
+    this.counter = 0;
   }
 }
