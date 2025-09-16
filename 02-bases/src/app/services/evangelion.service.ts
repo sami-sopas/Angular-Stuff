efdd575abd39c78c9d5f7171dddec30310a7616d
sami-sopas <ramirezcuenca@outlook.es>
2025-09-16 17:38:14 -0600

    read and wrrite from local storage and effects

--- 02-bases/src/app/services/evangelion.service.ts
+++ 02-bases/src/app/services/evangelion.service.ts
@@ -1,5 +1,11 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({providedIn: 'root'})
 export class EvangelionService {
@@ -7,10 +13,14 @@ export class EvangelionService {
   /* Tener la informacion en el servicio, permite que se mantenga aun cuando el componente que la usa se destruya
      es decir, podre cambiar de pagina y volver y la informacion seguira ahi */
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Shinji', age: 14 },
-    { id: 2, name: 'Rei', age: 15 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  //Cada que la señal cambie, se ejecutara este metodo, en este caso cuando this.characters() cambie
+  saveToLocalStorage = effect( () => {
+    console.log(`Character count ${this.characters().length}`);
+
+    localStorage.setItem('characters', JSON.stringify(this.characters()) ); //JSON.stringify convierte un objeto a string
+  })
 
   addCharacter(character: Character):void {
     this.characters.update(
