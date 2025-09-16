1ea7b42adfce3d30d98e3dd6bc716774fa54d46e
sami-sopas <ramirezcuenca@outlook.es>
2025-09-16 14:37:12 -0600

    inputs

--- 02-bases/src/app/pages/evangelion/evangelion.component.ts
+++ 02-bases/src/app/pages/evangelion/evangelion.component.ts
@@ -13,6 +13,9 @@ interface Character {
 })
 export class EvangelionComponent {
 
+  name = signal('Ikari');
+  age = signal(20);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Shinji', age: 14 },
     { id: 2, name: 'Rei', age: 15 },
@@ -20,6 +23,31 @@ export class EvangelionComponent {
     { id: 4, name: 'Kaworu', age: 14 },
     { id: 5, name: 'Toji', age: 12 },
   ]);
+
+  addCharacter():void {
+    if(!this.name() || this.age() <= 0){
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      age: this.age()
+    }
+
+    //this.characters().push(newCharacter); <- Esta opcion funciona, pero no es la mas recomendada
+
+    this.characters.update(
+      (list) => [...list, newCharacter]
+    );
+
+    this.resetFields();
+  }
+
+  resetFields(): void {
+    this.name.set('');
+    this.age.set(0);
+  }
 
   // ageClassess = computed( () => {
   //   return {
