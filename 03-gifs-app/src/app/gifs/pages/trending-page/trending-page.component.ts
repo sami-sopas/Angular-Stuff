4cea8febeb8db88f10b1018e244fbffeae6b35be
sami-sopas <ramirezcuenca@outlook.es>
2025-11-09 15:25:58 -0600

    retrieve scrollHeight in service when moving to another page

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,22 +1,33 @@
-import { Component, computed, ElementRef, inject, signal, viewChild } from '@angular/core';
-import { ListComponent } from "../../components/list/list.component";
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
+//import { ListComponent } from "../../components/list/list.component";
 import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-sate.service';
 
 @Component({
   selector: 'app-trending-page',
   //imports: [ListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
 
   //Services
   gifsService = inject(GifService);
 
+  scrollStateService = inject(ScrollStateService);
+
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  //Metodo que se ejecuta cuando ya se inicializo la vista
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if(!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
-
     if(!scrollDiv) return;
 
     const scrollTop = scrollDiv.scrollTop; //Posicion actual del scroll
@@ -25,6 +36,8 @@ export default class TrendingPageComponent {
 
     //Determinar si el scroll esta por llegar al final
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
     if(isAtBottom){
      this.gifsService.loadTrendingGifs();
