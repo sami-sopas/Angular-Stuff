7b838d5ae73ea65822768385482218dfe9fc9d90
sami-sopas <ramirezcuenca@outlook.es>
2025-11-17 17:18:49 -0600

    separate route file

--- 04-country-app/src/app/app.routes.ts
+++ 04-country-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { countryRoutes } from './country/country.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes')
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+
+
+];
+
+export default countryRoutes;
