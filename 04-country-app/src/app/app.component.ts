e62561525ada25c7f0522253bd9ebe35fc8f474f
sami-sopas <ramirezcuenca@outlook.es>
2025-11-17 17:42:32 -0600

    add footer

--- 04-country-app/src/app/app.component.ts
+++ 04-country-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'country-app';
